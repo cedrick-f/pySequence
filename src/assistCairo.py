0a6ab3e00944800cb2763f46b2138405e50e5f74
cedrick.faury@ac-clermont.fr <cedrick.faury@ac-clermont.fr@bb2b00d3-10d1-1af7-ec8d-8a111b5e6bf4>
2011-10-18 08:06:20 +0000

    Ajout branche "Systèmes"

--- src/assistCairo.py
+++ src/assistCairo.py
@@ -8,33 +8,54 @@ Copyright (C) 2011
 @author: Cedrick FAURY
 """
 import textwrap
-from math import sqrt
+from math import sqrt, pi
 
-def show_text_rect(ctx, texte, x, y, w, h, va = 'c', ha = 'c', b = 0.2):
+def show_text_rect(ctx, texte, x, y, w, h, va = 'c', ha = 'c', b = 0.2, orient = 'h'):
     """ Renvoie la taille de police et la position du texte
         pour qu'il rentre dans le rectangle
+        x, y, w, h : position et dimensions du rectangle
+        va, ha : alignements vertical et horizontal ('h', 'c', 'b' et 'g', 'c', 'd')
+        b : écart du texte par rapport au bord (relativement aux dimension du rectangle)
+        orient : orientation du texte ('h', 'v')
     """
 #    print "show_text_rect", texte
 
+    if orient == 'v':
+        ctx.rotate(-pi/2)
+        show_text_rect(ctx, texte, -y-h, x, h, w, va, ha, b)
+        ctx.rotate(pi/2)
+        return
+    
+    #
+    # "réduction" du réctangle
+    #
     x, y = x+w*b/2, y+h*b/2
     w, h = w*(1-b), h*(1-b)
  
-    ratioRect = 1.0*h/w
-     
+    
+    #
+    # Estimation de l'encombrement du texte (pour une taille de police de 1)
+    # 
     ctx.set_font_size(1)
     fascent, fdescent, fheight, fxadvance, fyadvance = ctx.font_extents()
     xbearing, ybearing, width, height, xadvance, yadvance = ctx.text_extents(texte)
     volumeTexte = 1.0*width*fheight
-    
+
+    ratioRect = 1.0*h/w
     W = sqrt(volumeTexte/ratioRect)
     H = ratioRect*W
     
+    #
+    # Découpage du texte
+    #
     nLignes = max(1,int(width/W))
-#    print "nLignes",nLignes
     wrap = len(texte)/nLignes
-    
     lt = textwrap.wrap(texte, wrap)
     nLignes = len(lt)
+    
+    #
+    # Calcul de la taille de police nécessaire pour que ça rentre
+    #
     maxw = 0
     for t in lt:
         xbearing, ybearing, width, height, xadvance, yadvance = ctx.text_extents(t)
@@ -46,15 +67,21 @@ def show_text_rect(ctx, texte, x, y, w, h, va = 'c', ha = 'c', b = 0.2):
     ctx.set_font_size(fontSize)
     fascent, fdescent, fheight, fxadvance, fyadvance = ctx.font_extents()
     
+    
+    #
+    # On dessine toutes les lignes de texte
+    #
     l = 0
     dy = (h-(fheight+fdescent)*nLignes)/2
 #    print "dy", dy
+    
     for t in lt:
 #        print "  ",t
         xbearing, ybearing, width, height, xadvance, yadvance = ctx.text_extents(t)
         xt, yt = x+xbearing+(w-width)/2, y-ybearing+fheight*l+dy+height/2
 #        print "  ",xt, yt
         ctx.move_to(xt, yt)
+        
         ctx.show_text(t)
         l += 1
     
