e4807af4b67e082bbb21e23cf5f4a453069cb55a
cedrick.faury@ac-clermont.fr <cedrick.faury@ac-clermont.fr@bb2b00d3-10d1-1af7-ec8d-8a111b5e6bf4>
2011-11-24 10:34:59 +0000

    Ajout prérequis de type séquence

--- src/recup_excel.py
+++ src/recup_excel.py
@@ -73,40 +73,40 @@ def getColonne(sel = None, c = None):
         return zip(*sel)[c]
         
 
-# ouverture du fichier Excel 
-wb = xlrd.open_workbook('CI_savoirs.xls')
- 
-# lecture des donnï¿½es dans les feuilles
-sh1 = wb.sheet_by_name(u'Feuil1')
-
-# lecture par colonne feuil1
-f1colonne2 = sh1.col_values(1)
-
-CI = ['CI1','CI2','CI3','CI4','CI5','CI6','CI7','CI8','CI9','CI10','CI11','CI12','CI13','CI14','CI15']
-# Cellule de dÃ©part D7(excel) - col3 lig6(python)
-Numcol = 3
-for n in range (1,14,1): # boucle sur colonnes
-    CI[n] = []
-    for Numlign in range(6,124,1): # boucle sur lignes
-        if sh1.col_values(Numcol)[Numlign] == 1 :
-            CI[n].append(f1colonne2[Numlign])
-    Numcol = Numcol + 1
-       
-premiere_col = 3 
-premiere_lig = 7
-nbr_lig = 117
-def get_savoir(ci):
-    lst = []
-    for Numlign in range(nbr_lig): # boucle sur nbr_lig lignes
-        if sh1.col_values(premiere_col+ci-1)[premiere_lig+Numlign] != "" :
-            lst.append(f1colonne2[premiere_lig+Numlign])
-    
-    return lst
-
-print get_savoir(3)
-#print CI[1]
-#print CI[2]
-#print CI[3]
+## ouverture du fichier Excel 
+#wb = xlrd.open_workbook('CI_savoirs.xls')
+# 
+## lecture des donnï¿½es dans les feuilles
+#sh1 = wb.sheet_by_name(u'Feuil1')
+#
+## lecture par colonne feuil1
+#f1colonne2 = sh1.col_values(1)
+#
+#CI = ['CI1','CI2','CI3','CI4','CI5','CI6','CI7','CI8','CI9','CI10','CI11','CI12','CI13','CI14','CI15']
+## Cellule de dÃ©part D7(excel) - col3 lig6(python)
+#Numcol = 3
+#for n in range (1,14,1): # boucle sur colonnes
+#    CI[n] = []
+#    for Numlign in range(6,124,1): # boucle sur lignes
+#        if sh1.col_values(Numcol)[Numlign] == 1 :
+#            CI[n].append(f1colonne2[Numlign])
+#    Numcol = Numcol + 1
+#       
+#premiere_col = 3 
+#premiere_lig = 7
+#nbr_lig = 117
+#def get_savoir(ci):
+#    lst = []
+#    for Numlign in range(nbr_lig): # boucle sur nbr_lig lignes
+#        if sh1.col_values(premiere_col+ci-1)[premiere_lig+Numlign] != "" :
+#            lst.append(f1colonne2[premiere_lig+Numlign])
+#    
+#    return lst
+#
+#print get_savoir(3)
+##print CI[1]
+##print CI[2]
+##print CI[3]
 
 
 
