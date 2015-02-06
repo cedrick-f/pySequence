91953e039c3094d08fe418f253cd83840a80b242
cedrick.faury@ac-clermont.fr <cedrick.faury@ac-clermont.fr@bb2b00d3-10d1-1af7-ec8d-8a111b5e6bf4>
2015-02-06 08:57:24 +0000

    Mise à jour référentiels STS Mise à jour du setup Ajout BO STS

--- src/setup.py
+++ src/setup.py
@@ -24,18 +24,55 @@ shutil.rmtree("build", ignore_errors=True)
 includefiles = [('Microsoft.VC90.CRT', "Microsoft.VC90.CRT"),
                      'gpl.txt', 
                      'fichier_prj.ico', 'fichier_seq.ico',
-                     'etablissements.txt',
+                     'etablissements.xml',
                      ('../tables', "../tables"),
                      ('../BO', "../BO"),
                      ('../referentiels', "../referentiels")]
 #includefiles.extend(glob(r"*.xlsx"))
 #includefiles.extend(glob(r"*.xls"))
 #includefiles.extend(glob(r"*.xlsm"))
+for p in ['C:\\Python27\\Lib\site-packages\\html5lib',
+            'C:\\Python27\\Lib\\site-packages\\xhtml2pdf',
+            'C:\\Python27\\Lib\\site-packages\\PIL']:
+    if not p in sys.path:
+        sys.path.append(p)
+
+#sys.path = ['', 'C:\\Python27\\Lib\\idlelib', 
+#            'C:\\Python27\\lib\\site-packages\\setuptools-3.6-py2.7.egg', 
+#            'C:\\Python27\\lib\\site-packages\\xmind-0.1a.0-py2.7.egg', 
+#            'C:\\Python27\\lib\\site-packages\\distribute-0.7.3-py2.7.egg', 
+#            'C:\\Python27\\lib\\site-packages\\six-1.8.0-py2.7.egg', 
+#            'C:\\Python27\\lib\\site-packages\\python_dateutil-2.2-py2.7.egg', 
+#            'C:\\Python27\\lib\\site-packages\\comtypes-1.1.1-py2.7.egg', 
+#            'C:\\Python27\\lib\\site-packages\\xlutils-1.7.1-py2.7.egg', 
+#            'C:\\Python27\\lib\\site-packages\\pillow-2.7.0-py2.7-win32.egg', 
+#            'C:\\Python27\\lib\\site-packages\\html5lib-1.0b3-py2.7.egg', 
+#            'C:\\Python27\\lib\\site-packages\\xhtml2pdf-0.0.6-py2.7.egg', 
+#            'C:\\Python27\\python27.zip', 'C:\\Python27\\DLLs', 
+#            'C:\\Python27\\lib', 
+#            'C:\\Python27\\lib\\plat-win', 
+#            'C:\\Python27\\lib\\lib-tk', 
+#            'C:\\Python27', 
+#            'C:\\Python27\\lib\\site-packages', 
+#            'C:\\Python27\\lib\\site-packages\\win32', 
+#            'C:\\Python27\\lib\\site-packages\\win32\\lib', 
+#            'C:\\Python27\\lib\\site-packages\\Pythonwin', 
+#            'C:\\Python27\\lib\\site-packages\\wx-3.0-msw',
+#            'C:\\Python27\\Lib\site-packages\\html5lib',
+#            'C:\\Python27\\Lib\\site-packages\\xhtml2pdf',
+#            'C:\\Python27\\Lib\\site-packages\\Pillow-2.7.0-py2.7-win32\\PIL']
+
 
 
 # Dependencies are automatically detected, but it might need fine tuning.
 build_exe_options = {'build_exe': 'build/bin',
-                     "packages": ["os"], 
+                     "packages": ["os", "xhtml2pdf","html5lib"], 
+                     "includes": ["xhtml2pdf", "xhtml2pdf.pisa","html5lib", "xhtml2pdf.w3c"],
+                     "optimize" : 0,
+#                     "path" : ["../packages/html5lib"],#, "../packages/xhtml2pdf",  "../packages/xhtml2pdf/w3c"],
+#                     "zip_includes": [("../packages/html5lib/", "html5lib"),
+#                                      ("../packages/xhtml2pdf/w3c/css.py", "xhtml2pdf/w3c/css.py")],
+                     "namespace_packages" : ["xhtml2pdf", "html5lib", "xhtml2pdf.w3c"],
                      "excludes": ["tkinter",
                                   '_gtkagg', '_tkagg', 'bsddb', 'curses', 'pywin.debugger',
                                   'pywin.debugger.dbgcon', 'pywin.dialogs', 'tcl',
